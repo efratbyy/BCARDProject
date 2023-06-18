d89d599d96479e7d74e27233cf5a1b158985089d
efrat ben yosef <efratbyy@gmail.com>
2023-06-18 18:44:55 +0300

    Improvements-edit user

--- client/src/users/pages/EditUserPage.tsx
+++ client/src/users/pages/EditUserPage.tsx
@@ -1,19 +1,17 @@
 import React, { useEffect } from "react";
 import { useUser } from "../providers/UserProvider";
 import useHandleUsers from "../hooks/useHandleUsers";
-import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import useForm from "../../forms/hooks/useForm";
 import { Container } from "@mui/material";
 import UserForm from "../components/UserForm";
 import ROUTES from "../../routes/routesModel";
 import initialSignupForm from "../helpers/initialForms/initialSignupForm";
 import userEditSchema from "../models/joi-schema/userEditSchema";
-import { string } from "joi";
 import mapUserToModel from "../helpers/normalization/mapUserToModel";
 
 export const EditUserPage = () => {
-  const { user } = useUser();
-  const { userId } = useParams();
+  const { user, token } = useUser();
   const { handleGetUser, handelUpdateUser } = useHandleUsers();
   const navigate = useNavigate();
   const { value, ...rest } = useForm(
@@ -31,16 +29,16 @@ export const EditUserPage = () => {
   } = rest;
 
   useEffect(() => {
-    if (userId)
-      handleGetUser(userId).then((userFromServer) => {
-        if (user && user._id !== userFromServer?._id)
+    if (user)
+      handleGetUser(user._id).then((userFromServer) => {
+        if (user && user._id !== userFromServer!._id)
           return navigate(ROUTES.ROOT);
         if (userFromServer) {
           const modeledUser = mapUserToModel(userFromServer);
           setData(modeledUser);
         }
       });
-  }, [userId]);
+  }, [user]);
 
   if (!user) return <Navigate replace to={ROUTES.ROOT} />;
 
